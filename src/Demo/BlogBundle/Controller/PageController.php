446de5695ca5dcf6aeba7be28758649564411167
Jonathan Claros Santander <andrew.hhb.cs@gmail.com>
2013-04-12 21:37:20 -0400

    finishing second page
    finishing second page

--- src/Demo/BlogBundle/Controller/PageController.php
+++ src/Demo/BlogBundle/Controller/PageController.php
@@ -10,4 +10,10 @@ class PageController extends Controller
     {
         return $this->render('DemoBlogBundle:Page:index.html.twig');
     }
+    
+    public function aboutAction() 
+    {
+      return $this->render('DemoBlogBundle:Page:about.html.twig');
+    }
+    
 }
